f1fd3dd436d8d00ffe62dec7e4e5ee1e0daaf7a4
Spade <18681023+phspade@users.noreply.github.com>
2019-08-24 14:50:02 +0000

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# amass, subfinder, snapd, aquatone, project sonar, massdns, gobuster, masscan, nmap, sensitive.py, curl, CRLF-Injection-Scanner, otxurls, waybackurls, DirSearch, LinkFinder, VHostScan
+# amass, subfinder, snapd, aquatone, project sonar, gobuster, masscan, nmap, sensitive.py, curl, CRLF-Injection-Scanner, otxurls, waybackurls, DirSearch, LinkFinder, VHostScan
 
-telegram_bot=""
+
 passwordx=""
-telegram_id=""
 
 if [ ! -f ~/$1 ]; then
 	mkdir ~/$1
@@ -29,17 +28,28 @@ fi
 if [ ! -f ~/$1/waybackurls ]; then
 	mkdir ~/$1/waybackurls
 fi
-
 sleep 5
+
+message () {
+	telegram_bot=""	
+	telegram_id=""
+	alert="https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text="
+	[ -z $telegram_bot ] && [ -z $telegram_id ] || curl -g $alert$1 --silent > /dev/null
+}
+
+scanned () {
+	cat $1 | sort -u | wc -l
+}
+
 
 echo "[+] AMASS SCANNING [+]"
 if [ ! -f ~/$1/$1-amass.txt ]; then
 	amass enum -brute -active -d $1 -o ~/$1/$1-amass.txt -config ~/amass/config.ini
-	amasscan=`cat ~/$1/$1-amass.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Amass%20Found%20$amasscan%20subdomain(s)%20for%20$1" --silent > /dev/null
+	amasscan=`scanned ~/$1/$1-amass.txt`
+	message "Amass%20Found%20$amasscan%20subdomain(s)%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Amass%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Amass%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -47,11 +57,11 @@ sleep 5
 echo "[+] SUBFINDER SCANNING [+]"
 if [ ! -f ~/$1/$1-subfinder.txt ]; then
 	subfinder -d $1 -o ~/$1/$1-subfinder.txt
-	subfinderscan=`cat ~/$1/$1-subfinder.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=SubFinder%20Found%20$subfinderscan%20subdomain(s)%20for%20$1" --silent > /dev/null
+	subfinderscan=`scanned ~/$1/$1-subfinder.txt`
+	message "SubFinder%20Found%20$subfinderscan%20subdomain(s)%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Subfinder%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Subfinder%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -61,13 +71,13 @@ if [ ! -f ~/aquatone/$1/urls.txt ]; then
 	aquatone-discover -d $1
 	aquatone-scan -d $1 -p huge
 	for domains in `cat ~/aquatone/$1/urls.txt`; do domain="${domains#*://}"; domainx="${domain%/*}"; echo $domainx >> ~/$1/$1-aquatone.txt;done
-	aquatonescan=`cat ~/$1/$1-aquatone.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Aquatone%20Found%20$aquatonescan%20subdomain(s)%20for%20$1" --silent > /dev/null
+	aquatonescan=`scanned ~/$1/$1-aquatone.txt`
+	message "Aquatone%20Found%20$aquatonescan%20subdomain(s)%20for%20$1"
 	echo "[+] Done"
 else
 	for domains in `cat ~/aquatone/$1/urls.txt`; do domain="${domains#*://}"; domainx="${domain%/*}"; echo $domainx >> ~/$1/$1-aquatone.txt;done
-	aquatonescan=`cat ~/$1/$1-aquatone.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Aquatone%20Scanning%20for%20$1" --silent > /dev/null
+	aquatonescan=`scanned ~/$1/$1-aquatone.txt`
+	message "Skipping%20Aquatone%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -75,11 +85,11 @@ sleep 5
 echo "[+] SUBLIST3R SCANNING [+]"
 if [ ! -f ~/$1/$1-sublist3r.txt ]; then
 	python ~/Sublist3r/sublist3r.py -b -d $1 -o ~/$1/$1-sublist3r.txt
-	sublist3rscan=`cat ~/$1/$1-sublist3r.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Sublist3r%20Found%20$sublist3rscan%20subdomain(s)%20for%20$1" --silent > /dev/null
+	sublist3rscan=`scanned ~/$1/$1-sublist3r.txt`
+	message "Sublist3r%20Found%20$sublist3rscan%20subdomain(s)%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Sublist3r%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Sublist3r%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -88,11 +98,11 @@ sleep 5
 echo "[+] SCANNING SUBDOMAINS WITH PROJECT SONAR [+]"
 if [ ! -f ~/$1/$1-project-sonar.txt ]; then
 	pv ~/2019-07-26-1564183467-fdns_any.json.gz | pigz -dc | grep -e ".$1," | jq -r '.name' | sort -u | grep ".$1" >> ~/$1/$1-project-sonar.txt
-	projectsonar=`cat ~/$1/$1-project-sonar.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Project%20Sonar%20Found%20$projectsonar%20subdomain(s)%20for%20$1" --silent > /dev/null
+	projectsonar=`scanned ~/$1/$1-project-sonar.txt`
+	message "Project%20Sonar%20Found%20$projectsonar%20subdomain(s)%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Project%20Sonar%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Project%20Sonar%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -100,11 +110,11 @@ sleep 5
 echo "[+] GOBUSTER SCANNING [+]"
 if [ ! -f ~/$1/$1-gobuster.txt ]; then
 	gobuster dns -d $1 -t 100 -w all.txt --wildcard -o ~/$1/$1-gobust.txt
-	gobusterscan=`cat ~/$1/$1-gobust.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Gobuster%20Found%20$gobusterscan%20subdomain(s)%20for%20$1" --silent > /dev/null
+	gobusterscan=`scanned ~/$1/$1-gobust.txt`
+	message "Gobuster%20Found%20$gobusterscan%20subdomain(s)%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Gobuster%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Gobuster%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -122,7 +132,7 @@ sleep 5
 
 
 all=`cat ~/$1/$1-final.txt | wc -l`
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Almost%20$all%20Collected%20Subdomain(s)%20for%20$1" --silent > /dev/null
+message "Almost%20$all%20Collected%20Subdomain(s)%20for%20$1"
 sleep 3
 
 
@@ -153,15 +163,15 @@ for alive in `cat ~/$1/$1-all.txt`; do
 		fi
 	done
 done
-alivesu=`cat ~/$1/$1-allx.txt | sort -u | wc -l`
+alivesu=`scanned ~/$1/$1-allx.txt`
 cat ~/$1/$1-allx.txt | sort -u > ~/$1/$1-allz.txt
 rm ~/$1/$1-allx.txt
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=$alivesu%20alive%20domains%20out%20of%20$all%20domains%20in%20$1" --silent > /dev/null
+message "$alivesu%20alive%20domains%20out%20of%20$all%20domains%20in%20$1"
 sleep 5
 
 echo "[+] SCANNING CRLF [+]"
 python3 ~/CRLF-Injection-Scanner/crlf_scan.py -i ~/$1/$1-allz.txt -o ~/$1/$1-crlf.txt
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=CRLF%20Scanning%20done%20for%20$1" --silent > /dev/null
+message "CRLF%20Scanning%20done%20for%20$1"
 sleep 5
 
 echo "[+] COLLECTING ENDPOINTS [+]"
@@ -170,28 +180,28 @@ for urlz in `cat ~/$1/$1-allz.txt`; do
 		python ~/LinkFinder/linkfinder.py -i $protoc://$urlz -d -o ~/$1/endpoints/$protoc_$urlz-result.html
 	done
 done
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Done%20collecting%20endpoint%20in%20$1" --silent > /dev/null
+message "Done%20collecting%20endpoint%20in%20$1"
 sleep 5
 
 echo "[+] MASSDNS SCANNING [+]"
 massdns -r ~/massdns/lists/resolvers.txt -t CNAME ~/$1/$1-allz.txt -o S > $1/$1-massdns.txt
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Done%20Massdns%20CNAME%20Scanning%20for%20$1" --silent > /dev/null
+message "Done%20Massdns%20CNAME%20Scanning%20for%20$1"
 sleep 5
 
 echo "[+] PORT SCANNING [+]"
 cat ~/$1/$1-allz.txt | aquatone -ports xlarge -out ~/$1/$1-ports
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Done%20Aquatone%20Port%20Scanning%20for%20$1" --silent > /dev/null
+message "Done%20Aquatone%20Port%20Scanning%20for%20$1"
 sleep 5
 
 
 echo "[+] MASSCAN PORT SCANNING [+]"
 if [ ! -f ~/$1/$1-masscan.txt ]; then
 	echo $passwordx | sudo -S masscan -p1-65535 -iL ~/$1/$1-ip.txt --max-rate 10000 -oG ~/$1/$1-masscan.txt
-	mass=`cat $1/$1-ip.txt | wc -l`
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Masscan%20Scanned%20$mass%20IPs%20for%20$1" --silent > /dev/null
+	mass=`scanned $1/$1-ip.txt`
+	message "Masscan%20Scanned%20$mass%20IPs%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Masscan%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Masscan%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -200,12 +210,12 @@ sleep 5
 echo "[+] NMAP PORT SCANNING [+]"
 if [ ! -f ~/$1/$1-nmap.txt ]; then
 	nmap -sV -Pn -p- -iL ~/$1/$1-ip.txt --stylesheet ~/nmap-bootstrap.xsl -oA ~/$1/$1-nmap
-	nmaps=`cat ~/$1/$1-ip.txt | wc -l`
+	nmaps=`scanned ~/$1/$1-ip.txt `
 	xsltproc -o ~/$1/$1-nmap.html ~/nmap-bootstrap.xsl ~/$1/$1-nmap.xml
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Nmap%20Scanned%20$nmaps%20IPs%20for%20$1" --silent > /dev/null
+	message "Nmap%20Scanned%20$nmaps%20IPs%20for%20$1"
 	echo "[+] Done"
 else
-	curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Skipping%20Nmap%20Scanning%20for%20$1" --silent > /dev/null
+	message "Skipping%20Nmap%20Scanning%20for%20$1"
 	echo "[!] Skipping ..."
 fi
 sleep 5
@@ -213,26 +223,26 @@ sleep 5
 echo "[+] Scanning for Sensitive Files [+]"
 cp ~/$1/$1-allz.txt ~/$1-sensitive.txt
 python ~/sensitive.py -u ~/$1-sensitive.txt
-sens=`cat ~/$1-sensitive.txt | wc -l`
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Sensitive%20File%20Scanned%20$sens%20asset(s)%20for%20$1" --silent > /dev/null
+sens=`scanned ~/$1-sensitive.txt`
+message "Sensitive%20File%20Scanned%20$sens%20asset(s)%20for%20$1"
 rm $1-sensitive.txt
 sleep 5
 
 echo "[+] OTXURL Scanning for Archived Endpoints [+]"
 for u in `cat ~/$1/$1-allz.txt`;do echo $u | otxurls >> ~/$1/otxurls/$u.txt; done
 cat ~/$1/otxurls/* | sort -u >> ~/$1/otxurls/$1.txt 
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=OTXURL%20Done%20for%20$1" --silent > /dev/null
+message "OTXURL%20Done%20for%20$1"
 sleep 5
 
 echo "[+] WAYBACKURLS Scanning for Archived Endpoints [+]"
 for u in `cat ~/$1/$1-allz.txt`;do echo $u | waybackurls >> ~/$1/waybackurls/$u.txt; done
 cat ~/$1/waybackurls/* | sort -u >> ~/$1/waybackurls/$1.txt 
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=WAYBACKURLS%20Done%20for%20$1" --silent > /dev/null
+message "WAYBACKURLS%20Done%20for%20$1"
 sleep 5
 
 echo "[+] DirSearch Scanning for Sensitive Files [+]"
 for u in `cat ~/$1/$1-allz.txt`;do python3 ~/dirsearch/dirsearch.py -u $u --ext php,bak,txt,asp,aspx,jsp,html,zip,jar,sql -b -w ~/newlist.txt >> ~/$1/dirsearch/$u-dirsearch.txt;done
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=DirSearch%20Done%20for%20$1" --silent > /dev/null
+message "DirSearch%20Done%20for%20$1"
 sleep 5
 
 NMAP_FILE=~/$1/$1-nmap.gnmap
@@ -252,8 +262,8 @@ for test in `cat $1/$1-ip.txt`; do
 	done
 done
 vt=`ls ~/$1/virtual-hosts/* | wc -l`
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Virtual%20Host(s)%20found%20$vt" --silent > /dev/null
+message "Virtual%20Host(s)%20found%20$vt"
 rm ~/$1/tmp.txt
 sleep 5
 
-curl -g "https://api.telegram.org/bot$telegram_bot/sendmessage?chat_id=$telegram_id&text=Scanner%20Done%20for%20$1" --silent > /dev/null
+message "Scanner%20Done%20for%20$1"
