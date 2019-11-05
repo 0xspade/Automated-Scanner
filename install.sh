803d43dbabd22859760bebfeecc4be806d3bfef7
Spade <18681023+phspade@users.noreply.github.com>
2019-11-05 14:45:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 # amass, subfinder, snapd, aquatone, project sonar, grepcidr, gobuster, masscan, nmap, sensitive.py, curl, CRLF-Injection-Scanner, otxurls, waybackurls, DirSearch, LinkFinder, VHostScan
 
-apt-get install pv curl wget grepcidr snapd nmap masscan sublist3r
+apt-get install pv curl wget grepcidr snapd nmap masscan sublist3r pigz golang sublist3r xsltproc
 
 export PATH=$PATH:/snap/bin #setup snap
 service snapd start #starting snap services
@@ -15,34 +15,36 @@ go get github.com/OJ/gobuster
 go get github.com/lc/otxurls
 go get github.com/tomnomnom/waybackurls
 go get github.com/hacks/filter-resolved
+go get github.com/Ice3man543/SubOver
+go get github.com/haccer/subjack
+go get github.com/michenriksen/aquatone
+go get github.com/ffuf/ffuf
+go get -u github.com/OWASP/Amass/...
+gem install aquatone
+pip3 install dnsgen
 
-#Sensitive-File-Explorer
-git clone https://github.com/phspade/Sensitive-File-Explorer.git ~/tools/Sensitive-File-Explorer
-pip install BeautifulSoup os sys commands argparse base64
+# Findomain
+sudo wget https://github.com/Edu4rdSHL/findomain/releases/latest/download/findomain-linux -O /usr/bin/findomain
+sudo chmod +x /usr/bin/findomain
 
 #Massdns
 git clone https://github.com/blechschmidt/massdns.git ~/tools/massdns
 cd ~/tools/massdns
 make
-sudo cp ~/tools/massdns/bin/massdns /usr/bin
-
-#CRLF-Scanner
-git clone https://github.com/random-robbie/CRLF-Injection-Scanner.git ~/tools/CRLF-Injection-Scanner
-cd ~/tools/CRLF-Injection-Scanner
-pip3 install -r requirements.txt
+sudo ln -sf ~/tools/massdns/bin/massdns /usr/bin/massdns
 
 #dirsearch
 git clone https://github.com/maurosoria/dirsearch.git
+ln -sf ~/dirsearch/dirsearch.py /usr/bin/dirsearch
 
+#changeme
+git clone https://github.com/ztgrace/changeme ~/tools/changeme
+cd ~/tools/changeme
+python3 -m pip install -r requirements.txt
+python3 -m pip install -r dev-requirements.txt
 
 #linkfinder
 git clone https://github.com/GerbenJavado/LinkFinder.git ~/tools/LinkFinder
 cd ~/tools/LinkFinder
 python setup.py install
-pip3 install -r requirements.txt
-
-#vhostscan
-git clone https://github.com/codingo/VHostScan.git ~/tools/VHostScan
-cd ~/tools/VHostScan
-python3 setup.py install
 pip3 install -r requirements.txt
