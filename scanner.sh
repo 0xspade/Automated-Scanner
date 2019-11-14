dfe2fa69e6a123c8aa672e57910c28934200da39
Spade <18681023+phspade@users.noreply.github.com>
2019-11-14 15:14:27 +0800

    Update scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -267,7 +267,7 @@ echo "[+] Done collecting endpoint"
 sleep 5
 
 echo "[+] COLLECTING ENDPOINTS FROM GITHUB [+]"
-if [ ! -z $(cat ~/tools/.tokens) ]; then
+if [ ! -z $(cat ~/tools/.tokens) ] && [ -e ~/tools/.tokens ]; then
 	for url in `cat ~/recon/$1/$1-httprobe.txt | sed 's/http:\/\///g' | sed 's/https:\/\///g' | sort -u`; do 
 		python3 ~/tools/github-endpoints.py -d $url -s -r > ~/recon/$1/github-endpoints/$url.txt
 		sleep 3
